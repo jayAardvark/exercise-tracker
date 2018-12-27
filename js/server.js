ae57942e5c7b2f78f95a7495051cf78f12383a7a
jayAardvark <44345078+jayAardvark@users.noreply.github.com>
2018-12-26 21:31:41 -0800

    finally: progress!

--- js/server.js
+++ js/server.js
@@ -25,11 +25,9 @@ app.get('/', (req, res) => {
 app.use((req, res, next) => {
   return next({status: 404, message: 'not found'})
 })
-
 // Error Handling middleware
 app.use((err, req, res, next) => {
   let errCode, errMessage
-
   if (err.errors) {
     // mongoose validation error
     errCode = 400 // bad request
@@ -52,8 +50,8 @@ app.use((err, req, res, next) => {
 let userSchema = new mongoose.Schema ({
   username: String,
   count: Number,
-  //log: [{description: String, duration: String, date: String}]//may need to refactor this with an object
-  log: []
+  log: [{description: String, duration: String, date: String}]//may need to refactor this with an object
+  //log: []
 });
 let User = mongoose.model('User', userSchema);
 
@@ -62,41 +60,36 @@ let exerciseSchema = new mongoose.Schema ({
   username: String,
   description: String,
   duration: String, //may need to change this data-type
+  /*_id: {
+    type: String,
+    ref: 'User'
+  },*/
   _id: String,
   date: String 
 });
 let ExerciseLog = mongoose.model('ExerciseLog', exerciseSchema);
 
 
-
 //get username and return object with name and _id
-
 app.post('/api/exercise/new-user', (req,res,next)=> {
-  /*let userName = req.body.username; //weird redundancy(?) I had */
   let userName = new User ({
     username: req.body.username
   });
-  //const createUser = function(done) {
     userName.save((err,data)=> {
       if(err) {
-        console.log("error")
-        //return done(err)
+        console.log("error");
       }else {
-        console.log(data)
-        //return done(null,data)
+        console.log(data);
       }
     
-    })
-  //}
-  res.json(userName)
-  //console.log(createUser)
+    });
+  res.json(userName);
   next();
 });
 
 //create GET that returns array of all users in DB
 app.get('/api/exercise/users', (req,res,next)=> {
   User.find((err, data)=> {
-    //res.json(data);
     if(err) {
       console.log('error');
     }res.json(data);
@@ -104,123 +97,32 @@ app.get('/api/exercise/users', (req,res,next)=> {
   })
 });
 
-
-
-//add exercises 
+//add exercises
 app.post('/api/exercise/add', (req,res,next)=> {
-  //find by id, add description, duration, and if there's no date retrieve-&-add current date.
-  console.log("made it this far");
-  /*
-  console.log(req.body);
-  console.log(req.body.description);
-  console.log("before is 1st req.body");
-  */
   let description = req.body.description;
   let duration = req.body.duration;
-  let date = req.body.date;
-  let dateArray = [];
-  //variable below is meant for res.json when uploading an exercise
-  let jsonDate;
   let id = req.body.userId;
-  let foundUser;
-  
-  /*
-  User.findById(req.body.userId, (err, data)=> {
-    if (err){
-      console.log("error finding by ID");
-    }else {
-      let exercise = new ExerciseLog ({
-        username: data.username,
-        duration: duration,
-        //_id: String,
-        //date: Date 
-      });
-      res.json(exercise);
-      console.log(exercise);
-      console.log("****")
-    }
-  });
-  */
-  //console.log(foundUser);
-  //console.log("______________________");
-  
-  
-  //figure logic for if "date" field is empty
-  
-  if (date == '') {
-    let d = new Date();
-    date = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate();
-    //this is all a MESS!!! determine this logic AFTER accounting for dates actually typed by user
-    jsonDate = new Date(d.getFullYear(), d.getMonth(), d.getDate());
-    jsonDate = jsonDate.toDateString();
-    //console.log(req);
-    //access username so as to include in res.json
-    //res.json({"description": description, "duration": duration,"_id": req.body.userId,"date": jsonDate});
+  let userName;
+  let date;
+  if (req.body.date == "" || req.body.date == null) {
+    date = new Date();
+    date = date.toDateString();
   }else {
-    jsonDate = req.body.date.split("-");
+    date = req.body.date;
+    date = req.body.date.split("-");
     //note that the 2nd argument below must be subtracted by 1 to account for months starting with Jan = "0", Feb = "1", etc...
-    jsonDate = new Date(jsonDate[0],jsonDate[1]-1,jsonDate[2]);
-    jsonDate = jsonDate.toDateString();
-    //res.json({"username": req.body.username, "date": jsonDate});
-    console.log(jsonDate);
-    //access username so as to include in res.json
-
-
-    //console.log(foundUser);
-    console.log("***ABOVE IS FOUNDUSER***");
-    //res.json({"description": description, "duration": duration,"_id": req.body.userId,"date": jsonDate});
-    console.log(typeof jsonDate);
-    console.log("****JSONDATE****");
-    console.log(req.body);
-  };
-  
-    User.findById(req.body.userId, (err, data, next)=> {
-      console.log(data);
-      console.log(jsonDate);
-      console.log("booooooo");
-    if (err){
-      console.log("error finding by ID");
-    }else {
-      console.log("booooooo 2");
-      //res.json("hey")
-      foundUser = data.username;
-      let exercise = new ExerciseLog ({
-        username: data.username,
-        duration: duration,
-        //_id: String,
-        date: date 
-      });
-      console.log("booooooo 3");
-      //res.json(exercise);
-      console.log(exercise);
-      console.log("****");
-    }
-//      next();
-  });
-  
-  //think about whether this section is redundant or not.  can it be combined with the above-use of findById?
-  User.findById({_id: req.body.userId},(err, data)=> {
-    //try adding res.json here
-    console.log(data);
-    console.log("***DATA AFTER POST***");
-    console.log(data.username);
-    console.log(data);
-    //res.json({"data":data})
-    console.log("***************************");
-
+    date = new Date(date[0],date[1]-1,date[2]);
+    date = date.toDateString();
+  }
+  User.findById(id, (err, data)=> {
     if (err) {
-        console.log("made it this far 2");
       console.log("error");
     }else {
-      console.log("made it this far 3");
-      //console.log(data);
-      //data.log.push({description: description, duration: duration, date: date});
+      console.log(data.username);
+      userName = data.username;
+      //res.json({"username": userName, "description": description, "duration": duration, "_id": id, 'date': date});
       data.log = data.log.concat({description: description, duration: duration, date: date});
-      //data.log.push({description: description})
-      /*
-      console.log(data);
-      console.log("above is post-push");
-      */
+      res.json({"username": userName, "description": description, "duration": duration, "_id": id, "date": date});
       data.markModified(data.log);
       data.save((err,data)=> {
         if (err) {
@@ -228,17 +130,13 @@ app.post('/api/exercise/add', (req,res,next)=> {
           console.log(err);
           console.log("error saving");
         }else {
-          console.log("made it this far 4");
+          console.log("****");
           console.log(data);
         }
-      })
+      });
     }
   });
-  //res.json({"description":description, "duration": duration, "username": foundUser, "_id": id, "date": date});
-  next();
-});
-
-
+})
 
 
 
